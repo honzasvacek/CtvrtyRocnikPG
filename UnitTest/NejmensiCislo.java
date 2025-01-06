65b08c6a156d047900363d9867c0ffa83b3ff505
2021-e-svacek <jan.svacek@student.gyarab.cz>
2025-01-06 08:32:13 +0100

    UnitTest

---
+++ UnitTest/NejmensiCislo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NejmensiCislo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result = getSmaller(a, b);
+
+        System.out.println(result);
+    }
+
+    public static int getSmaller(int x, int y) {
+        return x < y ? x : y;
+    }
+}
